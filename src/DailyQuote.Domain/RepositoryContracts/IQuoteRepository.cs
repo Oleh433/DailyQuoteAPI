b023ca512b073bb2e9631f2307837d7bbe238ead
Oleh Pobyvanets <pobyvanets2005@gmail.com>
2025-03-03 16:49:43 +0200

    Add the Quote Repository

---
+++ src/DailyQuote.Domain/RepositoryContracts/IQuoteRepository.cs
@@ -0,0 +1,17 @@
+﻿using DailyQuote.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Domain.RepositoryContracts
+{
+    public interface IQuoteRepository
+    {
+        Task<List<Quote>> GetAllQuotesAsync();
+        Task<Quote?> GetQuoteByQuoteIdAsync(Guid quoteId);
+        Task AddQuoteAsync(Quote quote);
+        Task DeleteQuoteAsync(Quote quote);
+    }
+}
---
+++ src/DailyQuote.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DailyQuote.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Quote>().ToTable("Quotes");
+        }
+    }
+}
