671e9183af75fa4b3202f19ffdae118cfd21b9df
Oleh Pobyvanets <pobyvanets2005@gmail.com>
2025-03-03 14:36:45 +0200

    Quote Entiti is added

---
+++ src/DailyQuote.Domain/Entities/Quote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Domain.Entities
+{
+    public class Quote
+    {
+        public Guid QuoteId { get; set; }
+        public string? QuoteContent { get; set; }
+        public QuoteType? QuoteType { get; set; }
+        public DateTime? LastShownTime { get; set; }
+    }
+
+    public enum QuoteType
+    {
+        Motivational,
+        Humor,
+        Philosophical,
+        Love,
+        Life
+    }
+}
