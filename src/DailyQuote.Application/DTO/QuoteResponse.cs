5c6ed0673a1e632d3d0ad144afee8903c6fef16b
Oleh433 <120088754+Oleh433@users.noreply.github.com>
2025-03-03 20:45:35 +0200

    DQ-19: Add the Quote Controller (#6)

---
+++ src/DailyQuote.Application/DTO/QuoteResponse.cs
@@ -0,0 +1,29 @@
+﻿using DailyQuote.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Application.DTO
+{
+    public class QuoteResponse
+    {
+        public Guid QuoteId { get; set; }
+        public string? QuoteContent { get; set; }
+        public QuoteType? QuoteType { get; set; }
+    }
+
+    public static class QuoteExtensions
+    {
+        public static QuoteResponse ToQuoteResponse(this Quote quote)
+        {
+            return new QuoteResponse
+            { 
+                QuoteId = quote.QuoteId, 
+                QuoteContent = quote.QuoteContent, 
+                QuoteType = quote.QuoteType 
+            };
+        }
+    }
+}
---
+++ src/DailyQuote.Application/DTO/QuoteUpdateRequest.cs
@@ -0,0 +1,26 @@
+﻿using DailyQuote.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Application.DTO
+{
+    public class QuoteUpdateRequest
+    {
+        public Guid QuoteId { get; set; }
+        public string? QuoteContent { get; set; }
+        public QuoteType? QuoteType { get; set; }
+
+        public Quote ToQuote()
+        {
+            return new Quote()
+            {
+                QuoteId = QuoteId,
+                QuoteContent = QuoteContent,
+                QuoteType = QuoteType
+            };
+        }
+    }
+}
