bfc443f41eff6324e5daa39a9a0093e945211599
Oleh433 <120088754+Oleh433@users.noreply.github.com>
2025-03-03 16:51:04 +0200

    DQ-16: Add the Quote Repository (#3)

---
+++ src/DailyQuote.Infrastructure/Repositories/QuoteRepository.cs
@@ -0,0 +1,43 @@
+﻿using DailyQuote.Domain.Entities;
+using DailyQuote.Domain.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyQuote.Infrastructure.Repositories
+{
+    public class QuoteRepository : IQuoteRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public QuoteRepository(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public async Task<List<Quote>> GetAllQuotesAsync()
+        {
+            return await _db.Quotes.ToListAsync();
+        }
+
+        public async Task<Quote?> GetQuoteByQuoteIdAsync(Guid quoteId)
+        {
+            return await _db.Quotes.FirstOrDefaultAsync(quote => quote.QuoteId == quoteId);
+        }
+
+        public async Task AddQuoteAsync(Quote quote)
+        {
+            _db.Quotes.Add(quote);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteQuoteAsync(Quote quote)
+        {
+            _db.Quotes.Remove(quote);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
