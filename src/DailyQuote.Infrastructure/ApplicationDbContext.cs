273bf8ca3910993b8c6ff4a29fc4d449e6e1ec0a
Oleh433 <120088754+Oleh433@users.noreply.github.com>
2025-03-03 18:58:36 +0200

    DQ-17: Connect Database (#4)

--- src/DailyQuote.Infrastructure/ApplicationDbContext.cs
+++ src/DailyQuote.Infrastructure/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace DailyQuote.Infrastructure
     {
         public DbSet<Quote> Quotes { get; set; }
 
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ src/DailyQuote.Infrastructure/Migrations/20250303164634_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyQuote.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    QuoteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuoteContent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuoteType = table.Column<int>(type: "int", nullable: true),
+                    LastShownTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.QuoteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}
